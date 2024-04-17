59281b63bc3068ee3c31e12ea36a5ced97b59404
titoni-andrea <andreatitoni@gmail.com>
2024-04-17 16:42:10 +0200

    finito tutte le età

--- main.js
+++ main.js
@@ -9,5 +9,11 @@ let costTicket = NumKm * costKm
 console.log(`Il costo base del biglietto è: ${costTicket}`)
 
 if (ageUser < 18) {
-    console.log(`Il costo del biglietto per te che sei minorenne è: ${costTicket - ((costTicket/100) * 20)}`)
+    console.log(`Il costo del biglietto per te che sei minorenne è: ${costTicket - ((costTicket / 100) * 20)}`)
+}
+else if (ageUser > 65) {
+    console.log(`Il costo del biglietto per te che hai più di 65 anni è: ${costTicket - ((costTicket / 100) * 40)}`)
+}
+else {
+    console.log(`Il costo del biglietto intero è: ${costTicket}`)
 }
