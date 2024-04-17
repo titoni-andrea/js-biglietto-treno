61f9bc97e0c30f7f13b877de1eba74e30973b9e0
titoni-andrea <andreatitoni@gmail.com>
2024-04-17 16:49:42 +0200

    stampare in pagina  il risultato

--- main.js
+++ main.js
@@ -6,14 +6,14 @@ let ageUser = prompt("Quanti anni hai?")
 
 let costTicket = NumKm * costKm
 
-console.log(`Il costo base del biglietto è: ${costTicket}`)
+document.getElementById("titolo").innerHTML = `Il costo base del biglietto è: ${costTicket}`
 
 if (ageUser < 18) {
-    console.log(`Il costo del biglietto per te che sei minorenne è: ${costTicket - ((costTicket / 100) * 20)}`)
+    document.getElementById("titoletto").innerHTML = `Il costo del biglietto per te che sei minorenne è: ${costTicket - ((costTicket / 100) * 20)}`
 }
 else if (ageUser > 65) {
-    console.log(`Il costo del biglietto per te che hai più di 65 anni è: ${costTicket - ((costTicket / 100) * 40)}`)
+    document.getElementById("titoletto").innerHTML = `Il costo del biglietto per te che hai più di 65 anni è: ${costTicket - ((costTicket / 100) * 40)}`
 }
 else {
-    console.log(`Il costo del biglietto intero è: ${costTicket}`)
+    document.getElementById("titoletto").innerHTML = `Il costo del biglietto intero è: ${costTicket}`
 }
