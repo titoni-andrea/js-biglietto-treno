4e5af5097c86cabaf97411e3ba1aa09cc29971af
titoni-andrea <andreatitoni@gmail.com>
2024-04-17 16:59:46 +0200

    Verifica se non vengono compilati dei dati

--- main.js
+++ main.js
@@ -6,9 +6,20 @@ let ageUser = prompt("Quanti anni hai?")
 
 let costTicket = NumKm * costKm
 
+const senzaDato = "Devi inserire dei dati nel banner per avere il tuo prezzo del biglietto!"
+
 document.getElementById("titolo").innerHTML = `Il costo base del biglietto è: ${costTicket.toFixed(2)}€`
 
-if (ageUser < 18) {
+
+if (NumKm == 0 || NumKm == null) {
+    document.getElementById("titoletto").innerHTML = senzaDato
+}
+
+else if (ageUser == 0 || ageUser == null) {
+    document.getElementById("titoletto").innerHTML = senzaDato
+}
+
+else if (ageUser < 18) {
     document.getElementById("titoletto").innerHTML = `Il costo del biglietto per te che sei minorenne è: ${(costTicket - ((costTicket / 100) * 20)).toFixed(2)}€`
 }
 else if (ageUser > 65) {
